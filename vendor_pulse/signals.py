2b502702667edae026282404d20b39b10e7f78fa
Leo Varghese <leovarghese@Leos-MacBook-Pro-2.local>
2024-05-04 12:13:07 +0530

    fix: signals comparing old data with new instead of update field

--- vendor_pulse/signals.py
+++ vendor_pulse/signals.py
@@ -28,20 +28,27 @@ def create_performance_history(sender, instance: Vendor, **kwargs):
     instance.create_historical_performance()
 
 
+@receiver(pre_save, sender=PurchaseOrder)
+def save_updated_fields(sender, instance: PurchaseOrder, **kwargs):
+    if instance.pk is None:
+        return
+
+    old_instance = PurchaseOrder.objects.get(pk=instance.pk)
+    instance._status = old_instance.status
+    instance._acknowledgment_date = old_instance.acknowledgment_date
+    instance._quality_rating = old_instance.quality_rating
+
+
 @receiver(post_save, sender=PurchaseOrder)
 def update_vendor_performance_metrics(sender, instance: PurchaseOrder, created, **kwargs):
-    if not created:
+    if created:
         return
 
     vendor = instance.vendor
-    update_fields = kwargs.get("update_fields", [])
-    if not update_fields:
-        return
-
-    should_update_on_time_delivery_rate = "status" in update_fields and instance.status == PurchaseOrder.COMPLETED
-    should_quality_rating_avg = "quality_rating" in update_fields
-    should_update_average_response_time = "acknowledgment_date" in update_fields
-    should_update_fulfillment_rate = "status" in update_fields
+    should_update_on_time_delivery_rate = instance._status != instance.status and instance.status == PurchaseOrder.COMPLETED
+    should_quality_rating_avg = instance._quality_rating != instance.quality_rating
+    should_update_average_response_time = instance._acknowledgment_date != instance.acknowledgment_date
+    should_update_fulfillment_rate = instance._status != instance.status
 
     if should_update_on_time_delivery_rate:
         vendor.update_on_time_delivery_rate()
